99e61728c31f6511f431532c584a2feebe47e29d
aminabdolahi <abdollahi.amin@gmail.com>
2023-07-22 12:11:14 +0330

    initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
-  )
+const Home = () => {
+    return (
+        <section className="flex flex-col paddings mb-16">
+            <h1>Categories</h1>
+            <h1>Posts</h1>
+            <h1>LoadMore</h1>
+        </section>
+    );
 }
+
+export default Home;
