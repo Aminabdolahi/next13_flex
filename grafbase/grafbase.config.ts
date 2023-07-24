1dffc4004f8a4ff514077cf275c9748d0451d94f
aminabdolahi <abdollahi.amin@gmail.com>
2023-07-24 10:06:48 +0330

    some bugs fixed

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -14,7 +14,11 @@ const User = g.model('User', {
 const Project = g.model('Project', {
   title: g.string().length({min: 3}),
   description: g.string(),
-  image: g.url()
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User)
 })
 
 export default config({
